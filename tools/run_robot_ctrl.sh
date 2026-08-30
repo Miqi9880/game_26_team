cb9702c6117c06229ba10fb5d63c16b49e8d0cdc
Miqi9880 <202500205018@stumail.sztu.edu.cn>
2026-08-31 05:13:09 +0800

    tools: fix robot_ctrl executable name (robot_ctrl_main, not robot_ctrl_node)

--- tools/run_robot_ctrl.sh
+++ tools/run_robot_ctrl.sh
@@ -5,5 +5,5 @@
 #   bash tools/run_robot_ctrl.sh
 #   bash tools/run_robot_ctrl.sh /dev/ttyUSB0
 DEV=${1:-/dev/robomaster}
-ros2 run serical_device_ros2 robot_ctrl_node --ros-args \
+ros2 run serical_device_ros2 robot_ctrl_main --ros-args \
   -p serial_device:=$DEV -p serial_enabled:=true -p dry_run:=false
